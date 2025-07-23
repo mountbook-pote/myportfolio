27790513369a682820c4543640ab445a43c7b881
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-07-23 16:43:54 +0900

    add user詳細画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   root 'homes#top'
 
+  # home
   get 'homes/top'
 
+  # user
   devise_for :users, controllers: {
   registrations: 'users/registrations'
   }
   devise_scope :user do
     post 'users/guest_login', to: 'users/sessions#guest_login'
   end
+  resources :users, only: [:show]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
