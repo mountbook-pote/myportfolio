db4bd29d9bf46d139a3619bb43b4e0ff524cfc4d
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-07-26 12:44:40 +0900

    add 各ジャンル用MetObjectメソッド追加

--- app/models/met_object.rb
+++ app/models/met_object.rb
@@ -1,20 +1,36 @@
 class MetObject < ApplicationRecord
-  # 予め指定したジャンルからランダムに指定枚取得
-  def self.fetch_met_objects_at_random(department_names = [], fetch_number)
-    met_works = []
+  EXTRA_FETCH_NUMBER = 2
 
-    department_names.each do |department_name|
+  def self.fetch_all_departments_works_at_random(all_department_names = [], fetch_each_number)
+    total_met_works = []
+
+    all_department_names.each do |department_name|
       records = where(department: department_name).
-        order('RANDOM()').limit(fetch_number * 2)
+        order('RANDOM()').limit(fetch_each_number * EXTRA_FETCH_NUMBER)
 
-      selected = []
+      filtered_records = []
       records.each do |record|
         next if record.primary_image_small.blank?
-        selected << record
-        break if selected.size >= fetch_number
+        filtered_records << record
+        break if filtered_records.size == fetch_each_number
       end
-      met_works += selected
+      total_met_works += filtered_records
     end
-    met_works
+    total_met_works
+  end
+
+  def self.fetch_department_works_at_random(department_name, fetch_number)
+    met_works = []
+
+    records = MetObject.where(department: department_name).
+      order('RANDOM()').limit(fetch_number * EXTRA_FETCH_NUMBER)
+
+    filtered_records = []
+    records.each do |record|
+      next if record.primary_image_small.blank?
+      filtered_records << record
+      break if filtered_records.size == fetch_number
+    end
+    met_works = filtered_records
   end
 end
