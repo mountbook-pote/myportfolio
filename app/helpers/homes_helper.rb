b2e86c82f07a0b0a4e17836da31479302f81beee
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-07-27 12:56:23 +0900

    fix departmentの実装を全てshowアクションで統一

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,2 +1,8 @@
 module HomesHelper
+  DEPARTMENTS_INFORMATION = [
+    { name: "全ジャンル", department_id: 0 },
+    { name: "ヨーロッパ絵画", department_id: 1 },
+    { name: "中世ヨーロッパ絵画", department_id: 2 },
+    { name: "古代エジプト美術", department_id: 3 },
+  ].freeze
 end
