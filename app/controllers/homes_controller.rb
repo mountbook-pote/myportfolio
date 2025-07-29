6e922aac8ff6050485cd5d0d525432a9a1c735c8
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-07-30 08:01:22 +0900

    add ホームに新着投稿を追加。

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,9 @@
 class HomesController < ApplicationController
+  FETCH_NUMBER = 6
+
   def top
+    @posts = Post.includes(:met_object, user: { image_attachment: :blob }).
+      order(created_at: :desc).
+      limit(FETCH_NUMBER)
   end
 end
