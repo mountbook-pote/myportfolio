ab2833d7b1e9eec2b27bf0f5299086a347b2aa10
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-01 15:23:02 +0900

    add ジャンル名を日本語に変換するメソッド追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,8 @@ module ApplicationHelper
       "#{page_title} - #{base_title}"
     end
   end
+
+  def ja_translated_name(en_department_name)
+    I18n.t("departments.#{en_department_name}", default: en_department_name)
+  end
 end
