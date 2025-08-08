c16e1835ec709f829c87283fd079222b1f030b0d
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-08 14:00:21 +0900

    fix 特定ページからの移動でセレクトボックスが反応しない不具合を修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,9 @@ import "channels"
 import $ from 'jquery'
 window.$ = $
 window.jQuery = $
+import "./select_department";
+import "./scroll_to_zero";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-document.addEventListener("turbolinks:load", function () {
-  window.scrollTo({ top: 0, left: 0, behavior: "instant" });
-});
--- app/javascript/packs/select_department.js
+++ app/javascript/packs/select_department.js
@@ -5,6 +5,7 @@ function goToDepartment() {
 }
 
 document.addEventListener('turbolinks:load', () => {
+  console.log('select_department.js loaded')
   const selectDepartmentButton = document.getElementById('select-department-button');
   if (!selectDepartmentButton) return;
   selectDepartmentButton.addEventListener('click', goToDepartment);
