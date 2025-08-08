5734958cc6f11dca67e2aa94cd8c746851b4058d
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-08 11:37:20 +0900

    add マイページのUI作成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def favorited_by_users_count(user)
+    favorited_sum = 0
+    user.posts.each do |post|
+      favorited = post.favorited_by_users.count
+      favorited_sum += favorited
+    end
+    favorited_sum
+  end
 end
