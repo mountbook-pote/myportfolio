25727dab7c5f4fbb13c3dac6e22c96c95746de48
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-09 05:14:59 +0900

    add 投稿やいいねが０の時にないことを表示するメソッド追加

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def check_posts_empty(posts)
+    return unless posts.empty?
+    if !current_page?(favorites_user_path)
+      "投稿はありません"
+    else
+      "いいねはありません"
+    end
+  end
 end
