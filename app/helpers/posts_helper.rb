c6c4d72fd26a24080b8720b7ec8ab9e4c4a3b204
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-09 06:22:04 +0900

    fix ０件表示メソッドで検索結果等が０件の時に不具合になるのを修正

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,2 @@
 module PostsHelper
-  def check_posts_empty(posts)
-    return unless posts.empty?
-    if !current_page?(favorites_user_path)
-      "投稿はありません"
-    else
-      "いいねはありません"
-    end
-  end
 end
