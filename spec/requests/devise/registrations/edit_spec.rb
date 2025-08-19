18b33f969937da73c6c26d085e0a3df71bc4cda2
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-19 16:46:27 +0900

    add devise用コントローラに追加した項目のテスト追加

--- spec/requests/devise/registrations/edit_spec.rb
+++ spec/requests/devise/registrations/edit_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Registration_edit", type: :request do
   describe "get users/edit" do
     let(:user) { create(:user, :with_icon_image) }
+    let(:no_image_user) { create(:user) }
     let(:guest_user) { User.guest }
 
     describe "リクエストの確認" do
@@ -50,14 +51,12 @@ RSpec.describe "Registration_edit", type: :request do
         expect(user.image).to be_attached
       end
 
-      it "ユーザ画像をデフォルトに戻すボタンが含まれる" do
-        expect(response.body).to include("元に戻す")
+      it "ユーザ画像選択ボタンが含まれる" do
+        expect(response.body).to include('class="icon-form"')
       end
 
-      it "ユーザ画像をデフォルトに戻すことができる" do
-        expect {
-          delete delete_icon_user_path(user)
-        }.to change { user.reload.image.attached? }.from(true).to(false)
+      it "ユーザ画像をデフォルトに戻すボタンが含まれる" do
+        expect(response.body).to include("元に戻す")
       end
 
       it "ユーザー名入力欄が含まれている" do
@@ -95,5 +94,85 @@ RSpec.describe "Registration_edit", type: :request do
         expect(response.body).to include('class="btn btn-danger"')
       end
     end  
+
+    describe "動作の確認(deviseに追加した項目)" do
+      context "ユーザログインした場合" do
+        before do
+          sign_in no_image_user
+          get edit_user_registration_path
+        end
+
+        it "名前を更新できる" do
+          patch user_registration_path, params: {
+            user: {
+              name: "new_name",
+              current_password: "password"
+            }
+          }
+          no_image_user.reload
+          expect(no_image_user.name).to eq("new_name")
+        end
+
+        it "ユーザアイコンを更新できる" do
+          image = fixture_file_upload(Rails.root.join("spec/fixtures/sample.jpg"), "image/jpg")
+          patch user_registration_path, params: {
+            user: {
+              image: image,
+              current_password: "password"
+            }
+          }
+          no_image_user.reload
+          expect(no_image_user.image).to be_attached
+          expect {
+            delete delete_icon_user_path(no_image_user)
+          }.to change { no_image_user.reload.image.attached? }.from(true).to(false)
+        end
+      end
+
+      context "ゲストログインした場合" do
+        before do
+          sign_in guest_user
+          get edit_user_registration_path
+        end
+
+        it "更新ができない" do
+          patch user_registration_path, params: {
+            user: {
+              name: "new_name",
+              current_password: "password"
+            }
+          }
+          guest_user.reload
+          expect(guest_user.name).to eq("ゲスト")
+        end
+
+        it "退会ができない" do
+          expect{
+            delete user_registration_path
+          }.not_to change(User, :count)
+        end
+      end
+
+      context "ログインしない場合" do
+
+        it "更新ができない" do
+          patch user_registration_path, params: {
+            user: {
+              name: "new_name",
+              current_password: "password"
+            }
+          }
+          expect(response).to redirect_to(new_user_session_path)
+          guest_user.reload
+          expect(user.name).to eq("user_name")
+        end
+
+        it "退会ができない" do
+          expect{
+            delete user_registration_path
+          }.not_to change(User, :count)
+        end
+      end
+    end
   end
 end
--- spec/requests/devise/registrations/new_spec.rb
+++ spec/requests/devise/registrations/new_spec.rb
@@ -10,25 +10,43 @@ RSpec.describe "Registration_page", type: :request do
       expect(response).to have_http_status(:success)
     end
 
-    it "ユーザー名入力欄が含まれている" do
-        expect(response.body).to include("ユーザー名を入力")
+    describe "内容の確認" do
+      it "ユーザー名入力欄が含まれている" do
+          expect(response.body).to include("ユーザー名を入力")
+      end
+
+      it "メールアドレス入力欄が含まれている" do
+        expect(response.body).to include("メールアドレスを入力")
+      end
+
+      it "パスワード入力欄が含まれている" do
+        expect(response.body).to include("パスワードを入力")
+      end
+
+      it "確認用パスワード入力欄が含まれている" do
+        expect(response.body).to include("確認用パスワードを入力")
+      end
+
+      it "新規登録ボタンが含まれている" do
+        expect(response.body).to include("新規登録")
+        expect(response.body).to include('class="btn btn-secondary"')
+      end
     end
 
-    it "メールアドレス入力欄が含まれている" do
-      expect(response.body).to include("メールアドレスを入力")
-    end
-
-    it "パスワード入力欄が含まれている" do
-      expect(response.body).to include("パスワードを入力")
-    end
-
-    it "確認用パスワード入力欄が含まれている" do
-      expect(response.body).to include("確認用パスワードを入力")
-    end
-
-    it "新規登録ボタンが含まれている" do
-      expect(response.body).to include("新規登録")
-      expect(response.body).to include('class="btn btn-secondary"')
+    describe "動作の確認(deviseに追加した項目)" do
+      it "名前を登録できる" do
+        expect{
+          post user_registration_path, params: {
+            user: {
+              name: "test001",
+              email: "test001@example.com",
+              password: "password",
+              password_confirmation: "password"
+            }
+          }
+        }.to change(User, :count).by(1)
+      expect(User.last.name).to eq("test001")
+      end
     end
   end
 end
