ba4032fe8b41fb07ceeb82e2f81fbad20a147757
mountbook <mountbook@mountbooknoMacBook-Air.local>
2025-08-20 10:41:28 +0900

    update userのmodel_specファイルにpostの内容が入っていたのを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "User(deviseに追加した項目)", type: :model do
-  describe "#guest" do
-    let!(:post_european){ create(:post, met_object: create(:met_object, department: "European Paintings")) }
-    let!(:post_medieval){ create(:post, met_object: create(:met_object, department: "Medieval Art")) }
-    let!(:post_egyptian){ create(:post, met_object: create(:met_object, department: "Egyptian Art")) }
-
-    context "params[:q]がnilの時" do
-      let(:params) { {} }
-
-      it "全ての作品を取得する" do
-        expect(Post.search_with_ransack(params)).to match_array(Post.all)
-      end
-    end
-
-    context "params[:q]のmet_object_department_inが空の時" do
-      let(:params){ {q: {met_object_department_in: ""} } }
-
-      it "作品の取得件数は0件となる" do
-        expect(Post.search_with_ransack(params)).to match_array(Post.none)
-      end
-    end
-  end
-
   describe "#self.guest" do
     let(:guest_user){ User.guest }
 
